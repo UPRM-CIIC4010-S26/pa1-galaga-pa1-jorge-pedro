7fa08e0d33a9578abdacbc2fe35470196ff2cde8
saruam21 <pedro.mauras@upr.edu>
2026-03-11 17:08:56 -0400

    made sound for enemies work correctly and edited the texture for Dyenemy

--- src/Entities/Enemies/Enemy.hpp
+++ src/Entities/Enemies/Enemy.hpp
@@ -53,6 +53,12 @@ class Enemy {
                     for (Projectile& p2 : Projectile::projectiles) {
                         if (p2.ID != 1 && HitBox::Collision(p.second->hitBox, p2.getHitBox())) {
                             p.second->health--;
+                            if(p.second -> health > 0){
+                                PlaySound(SoundManager::hit);
+                            }
+                            else {
+                                PlaySound(SoundManager::dead);
+                            }
                             p2.del = true;
                         }
                     }
